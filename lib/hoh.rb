0ded907b313a4e1fea9eddd6b4c284f04848946f
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-10-19 15:31:13 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -18,17 +18,17 @@
    label: "Order"
  }
  
-# PORTION_3 = {
-#   label: "Family",
-#   sub_category: {
-#    label: "Genus",
-#   }
-# }
-# 
-# PORTION_4 = {
-#   label: "Species",
-#   sub_category: nil
-# }
+ PORTION_3 = {
+   label: "Family",
+   sub_category: {
+    label: "Genus",
+   }
+ }
+ 
+ PORTION_4 = {
+   label: "Species",
+   sub_category: nil
+ }
 
 def naming_system
   # Remember:
